982e052c28dfb33ca5da2e9cdc90e7228d09d196
Mensahkwesiatta <mensahkwesiatta@gmail.com>
2022-10-13 18:40:30 -0700

    printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,1 +1,13 @@
-#include <>
+#include <stdio.h>
+
+/**
+ * main=prints a text
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
