71834f68fc877338b51b2a251457cb269bc1cf81
Mensahkwesiatta <mensahkwesiatta@gmail.com>
2022-10-13 18:07:52 -0700

    pep

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+
+/** 
+ * male - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual pauzzle"):
+	return(0);
+}
+
