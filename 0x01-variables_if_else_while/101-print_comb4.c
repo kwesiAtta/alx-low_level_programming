be52b58c123ac0d0e826b993b2f9995754bd60e8
kwesiAtta <mensahkwesiatta@gmail.com>
2022-10-17 16:00:22 -0700

    combination of 3

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -24,7 +24,7 @@ int main(void)
 					putchar(c);
 					putchar(b);
 					putchar(a);
-					if (!(a == '9' && c == '7' && b == '8'))
+					if (!(a == '9' && b == '8' && c == '7'))
 					{
 						putchar(',');
 						putchar(' ');
