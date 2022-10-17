92d374220430f984c361d5ceaa593306596f3996
kwesiAtta <mensahkwesiatta@gmail.com>
2022-10-17 13:01:41 -0700

    lowercase alphabets in reverse

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - numbers in reverse
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	char m;
+
+	m = 'z';
+	while
+		(m >= 'a') {
+			putchar(m);
+			m--;
+		}
+	putchar('\n');
+	return (0);
+}
