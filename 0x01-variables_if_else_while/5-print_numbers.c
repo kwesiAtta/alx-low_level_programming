fae084e0dea49f7da07eca66e070c8ec79139139
kwesiAtta <mensahkwesiatta@gmail.com>
2022-10-17 12:55:53 -0700

    numberz

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - base 10 numbers
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	int a;
+
+	for (a = 0; a <= 10; a++)
+		putchar(a);
+	putchar('\n');
+	return (0);
+}
