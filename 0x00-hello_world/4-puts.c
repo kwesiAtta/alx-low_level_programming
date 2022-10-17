3effef7368bc9e44b99d83b110952969e24fafa2
kwesiAtta <mensahkwesiatta@gmail.com>
2022-10-17 08:39:31 -0700

    hello

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,27 +1,13 @@
 #include <stdio.h>
-<<<<<<< HEAD
+
 /**
- * main - Entry point
- * Return: Always 0 (success)
-=======
-
-
-/** 
- * male - Entry point
+ * main - Entry Point
  *
- * Return: Always 0 (Success)
->>>>>>> b43cde56263ed46f67db77114f692b85f04003c2
+ * Return: Always 0 (success)
  */
-
 int main(void)
 {
-<<<<<<< HEAD
 	puts("\"Programming is like building a multilingual puzzle");
-	return(0);
-}
-=======
-	puts("\"Programming is like building a multilingual pauzzle");
-	return(0);
-}
+	return (0);
 
->>>>>>> b43cde56263ed46f67db77114f692b85f04003c2
+}
