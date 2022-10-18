7f1f1ad1dbc0ca5faeb6c6adcb4720abad8963ca
kwesiAtta <mensahkwesiatta@gmail.com>
2022-10-18 14:42:32 -0700

    task 3

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,11 +1,11 @@
 #include "main.h"
 
 /**
- * main - prints alphabet in lowercase
+ * print_alphabet - function to print alphabet in lowercase
  *
- * Return: Always 0
- *
+ * Return: Always 0 (success)
  */
+
 void print_alphabet(void)
 {
 	char ca;
---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,19 @@
+#include "main"
+
+/**
+ *print_alphabet_x10 - prints alphabets 10 times
+ */
+
+void print_alphabet_x10(void)
+{
+	int tens;
+	int ca;
+
+	for (tens = 0; tens <= 9; tens++)
+	{
+		for (ca = 'a'; ca <= 'z'; ca++)
+			_putchar(ca);
+
+		_putchar('\n');
+	}
+}
