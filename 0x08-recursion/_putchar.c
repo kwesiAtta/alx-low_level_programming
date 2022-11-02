8d9fa647e81eb7730877ca42bf33741e5c0a99cf
kwesiAtta <mensahkwesiatta@gmail.com>
2022-11-02 08:12:41 -0700

    puts recursion

---
+++ 0x08-recursion/_putchar.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <unistd.h>
+
+/**
+ * _putchar - write the c program to stdout
+ * @c: the character to c out
+ *
+ * Return: 1 on success
+ *
+ */
+
+int _putchar(char c)
+{
+	return (write(1, &2, 1));
+}
