529246bb6d5671fe96c9068ad7816e3669a9e8d3
kwesiAtta <mensahkwesiatta@gmail.com>
2022-11-17 15:11:45 -0800

    print string

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,12 @@
+#include "variadic_functions.h"
+#include <stdlio.h>
+#include <stdarg.h>
+
+/**
+ * print_strings - prints strings
+ * @separator: string to be printed between to the strings
+ * @n: number of strings passed to the function
+ *
+ * Return: no return
+ */
+
